ba95449c40bda68cfdbfc4f5697541910b4e13bc
bhbkk2001 <bhbkk2001@mail.ru>
2021-08-24 12:38:31 +0300

    Решил 2-ю задачу

--- 1-module/2-task/index.js
+++ 1-module/2-task/index.js
@@ -10,8 +10,15 @@ function print(text) {
  * чтобы функция sayHello работала корректно
  */
 function isValid(name) {
-  // ваш код...
+  if ( name === null || name.includes(' ') || name.length < 4 ){
+    return false
+  } else {
+    return true
+  } 
+  
 }
+  
+
 
 function sayHello() {
   let userName = prompt('Введите ваше имя');
