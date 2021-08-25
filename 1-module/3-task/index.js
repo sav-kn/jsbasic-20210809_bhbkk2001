796933508c2937546d02c5b3397608985ea29b9e
bhbkk2001 <bhbkk2001@mail.ru>
2021-08-25 19:08:17 +0300

    Задача (первый символ заглавный)

--- 1-module/3-task/index.js
+++ 1-module/3-task/index.js
@@ -1,3 +1,8 @@
 function ucFirst(str) {
-  // ваш код...
+  
+  if (str.length >= 1){
+    return str = str[0].toUpperCase() + str.substring(1)
+  } else if (str.includes('')){
+    return str.toUpperCase()
+  }
 }
