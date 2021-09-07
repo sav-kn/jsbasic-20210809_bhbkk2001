12bd6b58c0700ea81f32026c89336ded62b63699
bhbkk2001 <bhbkk2001@mail.ru>
2021-09-07 18:22:50 +0300

    решил 1 задачу 2 модуль

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,39 @@
 function sumSalary(salaries) {
-  // ваш код...
-}
+  let culc = 0;
+  
+    for (let key in salaries) {
+      if(Number.isFinite(salaries[key])){
+     culc += salaries[key];
+      }
+    }
+    return culc;
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*function sumSalary(salaries) {
+  let culc;
+  for (let key in sum) {
+    if(typeof salaries[key] === number && typeof salaries[key] !== NaN && typeof salaries[key]!== Infinity && typeof salaries[key] !== -Infinity && salaries[key] !== string ){
+      culc += salaries[key];
+    } else {
+      return 0;
+    }
+  }
+  return culc;
+ }
+*/
+
