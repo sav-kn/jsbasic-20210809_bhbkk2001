15ec86cd733b8af7fa1026b008ea1ded374738d3
bhbkk2001 <bhbkk2001@mail.ru>
2021-09-07 18:35:58 +0300

    решил 2 задачу 2 модуль

--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,13 @@
 function isEmpty(obj) {
   // ваш код...
+  let culc = 0;
+  for ( let key in obj ){
+    culc += obj[key];
+    
+  }
+  if (culc !== 0){
+    return false;
+  } else {
+    return true;
+  }
 }
