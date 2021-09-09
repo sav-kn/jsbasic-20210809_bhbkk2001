7209408519f7d96a20b82aed5262dda8301c763f
bhbkk2001 <bhbkk2001@mail.ru>
2021-09-09 15:01:04 +0300

    задача калькулятор

--- 2-module/3-task/index.js
+++ 2-module/3-task/index.js
@@ -1,5 +1,18 @@
 let calculator = {
-  // ваш код
+  // ваш кода
+  a: 0,
+  b: 0,
+
+  read(a, b){
+  this.a = a;
+  this.b = b;
+  },
+  sum: function (){
+    return this.a + this.b
+  },
+  mul: function (){
+    return this.a * this.b
+  }
 };
 
 // НЕ УДАЛЯТЬ СТРОКУ, НУЖНА ДЛЯ ПРОВЕРКИ
