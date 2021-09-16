e709f4845c08459925e5aab7cb844cc1b6aeea62
bhbkk2001 <bhbkk2001@mail.ru>
2021-09-16 19:42:22 +0300

    решил 3 задачу 3 модуля

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,3 +1,33 @@
 function camelize(str) {
   // ваш код...
+ if(str[0] === "-"){
+  let strNew = str.slice(1);
+  let arrStr = strNew.split('-');
+  let newArr = arrStr.map((word) => word[0].toUpperCase() + word.substring(1));
+  return str = newArr.join('');
+ } else if(str !== '') {
+  let arrStr = str.split('-');
+  let newArr = arrStr.map((word) => word[0].toUpperCase() + word.substring(1));
+ newArr.unshift(newArr[0].toLowerCase());
+ newArr.splice(1, 1);
+  return str = newArr.join('');
+ } else if(str <= 1){
+ return str;
+ }
 }
+
+
+
+
+
+
+
+
+
+
+
+/*
+let arrStr = str.split('-');
+let newArr = arrStr.map((word) => word[0].toUpperCase() + word.substring(1));
+return str = newArr.join('');
+*/
