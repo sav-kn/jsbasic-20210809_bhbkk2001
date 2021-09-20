e28b5372d10274450b771936d43e17ff7bd00ba1
bhbkk2001 <bhbkk2001@mail.ru>
2021-09-20 18:59:39 +0300

    решил 4 задачу 3 модуля

--- 3-module/4-task/index.js
+++ 3-module/4-task/index.js
@@ -1,3 +1,13 @@
 function showSalary(users, age) {
   // ваш код...
+  let strVl,
+      arrVl = [];
+  for (let i = 0; i < users.length; i++){
+    if(i = 0 && users[i].age === age){
+      arrVl +=  users[i].name + ', ' + users[i].balance; 
+    } else if (i > 0 && users[i].age <= age){
+      arrVl += '\n' + users[i].name + ', ' + users[i].balance; 
+    }
+  }
+  return strVl = arrVl;
 }
